5ffc00fb8c6fe8fa048086aeb709740fd858a606
Daniel Oram <danieloramnz@gmail.com>
2016-11-10 11:23:20 +1300

    add view controllers for first demo

---
+++ SideScrollCells/HorizontalSlideTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HorizontalSlideTableViewCell.swift
+//  SideScrollCells
+//
+//  Created by Daniel Oram on 9/11/16.
+//  Copyright © 2016 Daniel Oram. All rights reserved.
+//
+
+import UIKit
+
+class HorizontalSlideTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
