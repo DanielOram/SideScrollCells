6493bf74e311c44704e9d9288d7d928769a3045a
Daniel Oram <danieloramnz@gmail.com>
2016-11-13 16:37:14 +1300

    major modifications
    add flickr pod dependency
    add activity indicators
    add various code additions

--- SideScrollCells/HelperMethods.swift
+++ SideScrollCells/HelperMethods.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import Alamofire
 
 
 func generateRandomData() -> [[UIColor]] {
@@ -31,3 +32,47 @@ extension UIColor {
         return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1.0)
     }
 }
+
+
+//functions for calling flickr api
+
+func getImagesFromAPI() {
+    Alamofire.request("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ca4a2557448ed06d8f8548b96187ece4&format=json&nojsoncallback=1&auth_token=72157676448888075-c0f321170a03c27c&api_sig=16c5415ddae6b8109b90d22de89b66f3").responseJSON { response in
+        
+            //parse response json into images
+        
+    }
+}
+
+//static var for getting images
+
+class Assets {
+    
+    static var tempImages: [UIImage]!
+    
+    static var Images: [UIImage] {
+        get {
+            Alamofire.request("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ca4a2557448ed06d8f8548b96187ece4&format=json&nojsoncallback=1&auth_token=72157676448888075-c0f321170a03c27c&api_sig=16c5415ddae6b8109b90d22de89b66f3").responseJSON { response in
+                
+                //convert json data to images
+                tempImages = [UIImage]()
+            }
+            return tempImages
+        }
+        set {
+            
+        }
+    }
+}
+
+
+class PhotoMngr {
+    static let sharedInstance = PhotoMngr()
+    
+    var photos: [UIImage] = [UIImage]()
+    
+    private init() {}
+    
+}
+
+
