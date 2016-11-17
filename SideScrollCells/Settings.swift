32551e4e45ba9d4ff79b2aea0bf848fc9d53c6dd
Daniel Oram <danieloramnz@gmail.com>
2016-11-18 02:03:19 +1300

    setup function for getting localised data from appropriate file

--- SideScrollCells/Settings.swift
+++ SideScrollCells/Settings.swift
@@ -11,6 +11,7 @@ import Foundation
 class Settings {
     
     static let defaults = UserDefaults.standard
+    static var localisedData: NSArray?
     
     struct Keys {
         static let SETTINGS_LANGUAGE = "settings_language"
@@ -20,12 +21,27 @@ class Settings {
     
     static var settingsLanguage: String {
         get {
-            return defaults.string(forKey: self.Keys.SETTINGS_LANGUAGE)!
+            //check language has been set TODO: - prompt user for language!
+            guard let language: String = defaults.string(forKey: self.Keys.SETTINGS_LANGUAGE) else {
+                //prompt user to select language
+                
+                //return english for now
+                return "English"
+            }
+            return language
         }
         
         set(language) {
             defaults.set(language, forKey: self.Keys.SETTINGS_LANGUAGE)
         }
+    }
+    
+    //load localised strings file
+    
+    func loadLocalisedLanguage() {
+        
+        let localisedDataPath = Bundle.main.path(forResource: Settings.settingsLanguage, ofType: "plist")
+        Settings.localisedData = NSArray(contentsOfFile: localisedDataPath!)
     }
     
     
