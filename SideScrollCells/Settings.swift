427f2a6abb94e1c959268aa1a65277e120e57898
Daniel Oram <danieloramnz@gmail.com>
2016-11-18 01:54:33 +1300

    setup Userdefaults for settings

--- SideScrollCells/Settings.swift
+++ SideScrollCells/Settings.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+class Settings {
+    
+    static let defaults = UserDefaults.standard
+    
+    struct Keys {
+        static let SETTINGS_LANGUAGE = "settings_language"
+    }
+    
+    //the set language to localise for
+    
+    static var settingsLanguage: String {
+        get {
+            return defaults.string(forKey: self.Keys.SETTINGS_LANGUAGE)!
+        }
+        
+        set(language) {
+            defaults.set(language, forKey: self.Keys.SETTINGS_LANGUAGE)
+        }
+    }
+    
+    
+}
+
+
+
+
+
